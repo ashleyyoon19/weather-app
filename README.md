c47dc2aa597be8ce4b28224d5f2d7d74a7648355
ashleyyoon19 <ashley.yoon100@gmail.com>
2021-01-27 01:19:06 +0900

    Implemented search button, fixed errors, cleaned up code, added comments

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# weather-app
+# The Odin Project: Weather App
+
+Pulled the [OpenWeatherMap API](https://openweathermap.org/) & [IPData API](https://ipdata.co/) to create a user-friendly weather app using HTML, CSS, and Javascript.
+
+Features of the app:
+1. Detects city visitor is in via IP address and sets it to default in regards to their local date, time, and weather;
+2. Provides weather information, such as the description, wind speed, and humidity;
+3. Changes the weather icon based on the city's weather description;
+4. Changes the background image based on the visitor's local time;
+5. Provides a search bar for users to search other cities' weather.
--- script.js
+++ script.js
@@ -7,128 +7,92 @@ const humidity=document.querySelector('#humidity');
 const weatherImg=document.getElementById('weatherImg');
 const localDate=document.querySelector('#localDate');
 const localTime=document.querySelector('#localTime');
+const celsiusButton=document.querySelector('#celsius');
+const fahrenheitButton=document.querySelector('#fahrenheit');
+const userInput=document.querySelector('#search');
 
-localDate.innerHTML=new Date().toLocaleDateString();
-
-let tempUnit="celsius";
+//defining variables
 let kelvinToCelsius=0;
 let celsiusToFah=0;
-
-// let input=document.querySelector('#search');
-var clicked=false;
-const input=document.querySelector('#search');
-let userInput="";
-
 let nightColor=false;
+let loc="";
+let description="";
+let hours="";
 
+getTime();
+setInterval(getTime, 1000);
+getLocation();
+getDate();
+privacy();
+
+/*fetches API from OpenWeatherMap*/
 async function getWeather(location){
     const apiKey='ebbd521303ecc9a32317e8f713c1c8b1';
     const response=await fetch('https://api.openweathermap.org/data/2.5/weather?q='+location+'&appid='+apiKey, {mode: 'cors'});
 
     response.json().then(function(response){
         weatherLocation.innerHTML=response.name+", "+response.sys.country;
-        
+    
         kelvinToCelsius=Math.round(response.main.temp-273.15);
         celsiusToFah=Math.round((kelvinToCelsius*1.8)+32);
 
-        //if fahrenheit button is clicked, then...
-        if(tempUnit=="celsius"){
-            weatherTemp.innerHTML=kelvinToCelsius;
-        } else if(tempUnit=="fahrenheit"){
-            weatherTemp.innerHTML=celsiusToFah;
-        }
-        let description=response.weather[0].description;
+        weatherTemp.innerHTML=kelvinToCelsius;
+        if(nightColor==true) document.getElementById("fahrenheit").style.color="#808080";
 
-        if(description.includes("rain") && !description.includes("snow")){
-            if(nightColor==true){
-                weatherImg.src="images/rainy.png";
-            } else {
-                weatherImg.src="https://static.thenounproject.com/png/939134-200.png";
-            }
-            // document.body.style.backgroundImage="url('https://cdn.wallpapersafari.com/66/97/HonF9g.jpeg')";
-        } else if(description.includes("clear")){
-            if(nightColor==true){
-                weatherImg.src="images/sunny.png";
-            } else {
-                weatherImg.src="https://static.thenounproject.com/png/1355684-200.png";
-            }
-            // document.body.style.backgroundImage="url('https://cff2.earth.com/uploads/2017/11/08174338/How-ice-forms-inside-of-clouds.jpg')";
-        } else if(description.includes("few")){
-            if(nightColor==true){
-                weatherImg.src="images/whitecloud.png";
-            } else {
-                weatherImg.src="https://static.thenounproject.com/png/1416977-200.png";
-            }
-        } else if(description.includes("cloud") && !description.includes("few")){
-            if(nightColor==true){
-                weatherImg.src="images/cloudy.png";
-            } else {
-                weatherImg.src="https://static.thenounproject.com/png/1915084-200.png";
-            }
-            // document.body.style.backgroundImage="url('https://upload.wikimedia.org/wikipedia/commons/b/b9/Scattering_of_clouds.jpg')";
-        } else if(description.includes("snow")){
-            if(nightColor==true){
-                weatherImg.src="images/snow.png";
-            } else {
-                weatherImg.src="https://static.thenounproject.com/png/1350851-200.png";
-            }
-            // document.body.style.backgroundImage="url('https://media.cntraveler.com/photos/5c0840017732ca62ae9f72e9/16:9/w_1600,c_limit/Hallstatt%20GettyImages-899427986.jpg')";
-        } else if(description.includes("mist")){
-            if(nightColor==true){
-                weatherImg.src="images/misty.png";
-            } else {
-                weatherImg.src="https://static.thenounproject.com/png/1021681-200.png";
-            }
-        } else if(description.includes("thunder")){
-            if(nightColor==true){
-                weatherImg.src="images/thunder.png";
-            } else {
-                weatherImg.src="https://static.thenounproject.com/png/2266531-200.png";
-            }
-        }
+        description=response.weather[0].description;
+        changeImg(description);
+
         desc.innerHTML=description;
         wind.innerHTML=response.wind.speed+" m/s";
-        humidity.innerHTML=response.main.humidity+" %";
+        humidity.innerHTML=response.main.humidity+"%";
     })
 }
 
-//CLEAN UP CODE FOR TEMPCONVERSION + GETWEATHER() part
+/*changes weather icon based on weather description*/
+function changeImg(description){
+    if(description.includes("rain") && !description.includes("snow")){
+        nightColor==true ? weatherImg.src="images/rainy.png" : weatherImg.src="https://static.thenounproject.com/png/939134-200.png";
+    } else if(description.includes("clear")){
+        nightColor==true ? weatherImg.src="images/sunny.png" : weatherImg.src="https://static.thenounproject.com/png/1355684-200.png";
+    } else if(description.includes("few")){
+        nightColor==true ? weatherImg.src="images/whitecloud.png" : weatherImg.src="https://static.thenounproject.com/png/1416977-200.png";
+    } else if(description.includes("cloud") && !description.includes("few")){
+        nightColor==true ? weatherImg.src="images/cloudy.png" : weatherImg.src="https://static.thenounproject.com/png/1915084-200.png";
+    } else if(description.includes("snow")){
+        nightColor==true ? weatherImg.src="images/snow.png" : weatherImg.src="https://static.thenounproject.com/png/1350851-200.png";
+    } else if(description.includes("mist")){
+        nightColor==true ? weatherImg.src="images/misty.png" : weatherImg.src="https://static.thenounproject.com/png/1021681-200.png";
+    } else if(description.includes("thunder")){
+        nightColor==true ? weatherImg.src="images/thunder.png" : weatherImg.src="https://static.thenounproject.com/png/2266531-200.png";
+    }
+} 
 
+/*converts celsius to fahrenheit and vice versa depending
+on button clicked*/
 function tempConversion(button){
-    const celsiusButton=document.querySelector('#celsius');
-    const fahrenheitButton=document.querySelector('#fahrenheit');
     if(button.id=="celsius"){
         celsiusButton.style.color="#fff";
         weatherTemp.innerHTML=kelvinToCelsius;
-        if(nightColor==true){
-            fahrenheitButton.style.color="#808080";
-        } else {
-            fahrenheitButton.style.color="#000";
-        }
+        nightColor==true ? fahrenheitButton.style.color="#808080" : fahrenheitButton.style.color="#000";
     } else if(button.id=="fahrenheit"){
         fahrenheitButton.style.color="#fff";
         weatherTemp.innerHTML=celsiusToFah;
-        tempUnit="fahrenheit";
-        if(nightColor==true){
-            celsiusButton.style.color="#808080";
-        } else {
-            celsiusButton.style.color="#000";
-        }
+        nightColor==true ? celsiusButton.style.color="#808080" : celsiusButton.style.color="#000";
     }
 }
 
+/*gets local time of visitor/user*/
 function getTime(){
+    hours=new Date().getHours();
     let mins=new Date().getMinutes();
-    let hours=new Date().getHours();
-
     let localTime=document.querySelector('#localTime');
 
-    if(mins<10){
-        localTime.innerHTML=hours+":0"+mins;
-    } else {
-        localTime.innerHTML=hours+":"+mins;
-    }
+    mins<10 ? localTime.innerHTML=hours+":0"+mins : localTime.innerHTML=hours+":"+mins;
+    timeImg(hours);
+}
 
+/*changes background of app based on local time of visitor/user*/
+function timeImg(hours){
     if(hours>=5 && hours<8) {
         document.body.style.backgroundImage="url('https://images.hdqwalls.com/download/sunrise-on-the-beach-in-the-summer-time-at-ocean-isle-beach-4k-ga-1600x900.jpg')";
     } else if(hours>=8 && hours<=16){
@@ -140,36 +104,35 @@ function getTime(){
         weatherLocation.style.color="#fff";
         weatherTemp.style.color="#fff";
         localDate.style.color="#fff";
-        document.getElementById("calendar").src="calendar.png";
+        document.getElementById("calendar").src="images/calendar.png";
         document.getElementById("weaTitle").style.color="#fff";
         document.getElementById("border").style.color="#fff";
-        document.getElementById("fahrenheit").style.color="#808080";
         nightColor=true;
     }
 }
 
-getTime();
+/*gets local date of visitor/user*/
+function getDate(){
+    localDate.innerHTML=new Date().toLocaleDateString();
+}
 
-setInterval(getTime, 1000);
-
-let loc="";
-
+/*gets location of visitor via IP address*/
 async function getLocation(){
     const response=await fetch('https://api.ipdata.co/?api-key=test', {mode: 'cors'});
-
     response.json().then(function(response){
         loc=response.city;
         getWeather(loc);
-
-        const responseVar=response.time_zone.current_time;
-        // localDate.innerHTML=;
-        for(let i=0; i<responseVar.length; i++){
-            // if(resonseVar)
-        }
     })
 }
-getLocation();
 
-function getDate(){
+/*fetches weather API based on city visitor searches/enters in*/
+function searchLocation(button){
+    let inputValue=userInput.value;
+    getWeather(inputValue);
+}
 
+/*sets color of privacy info based on time*/
+function privacy(){
+    nightColor==true ? document.getElementById("privacyInfo").style.color="#fff" 
+    : document.getElementById("privacyInfo").style.color="#000"
 }
